be15c97be0b199b3caa1a9dc168df8f7cffc9b17
Angela <ambengeangela@gmail.com>
2023-06-15 09:39:01 +0300

    added js file

--- Q2-Assessment/ass.js
+++ Q2-Assessment/ass.js
@@ -6,4 +6,31 @@ class Recipe{
         this.cookingMethod = cookingMethod;
         this.nutritionalInfo = nutritionalInfo;
     }
+
 }
+
+class Product {
+    constructor(name, price, quantity) {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+    }
+    calculateTotal() {
+      return this.price * this.quantity;
+    }
+  }
+  let product1 = new Product("Laptop", 250000.99, 2);
+  let  product2 = new Product("mouse", 500.00, 2);
+
+  let totalValue1 = product1.calculateTotal();
+  let totalValue2 = product2.calculateTotal();
+  console.log(` ${product1.name}: $${totalValue1}`);
+  console.log(` ${product2.name}: $${totalValue2}`);
+
+  class Student{
+    constructor(name,age,grades){
+        this.name=name
+        this.age=age
+        this.grades=grades
+    }
+  }
